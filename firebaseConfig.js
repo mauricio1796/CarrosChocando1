f9b14b5fbc3627908bb08db88b73913b7303c238
mauricio1796 <ricardo.mosquerab@uniagustiniana.edu.co>
2025-03-26 21:42:38 +0000

    integracion base de datos

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_AUTH_DOMAIN",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_STORAGE_BUCKET",
-  messagingSenderId: "TU_MESSAGING_SENDER_ID",
-  appId: "TU_APP_ID"
+    apiKey: "AIzaSyBSNycbRFZ0HtKTzwzvfRiqWeYDkJ3oQaE",
+    authDomain: "base-1-d1bae.firebaseapp.com",
+    projectId: "base-1-d1bae",
+    storageBucket: "base-1-d1bae.firebasestorage.app",
+    messagingSenderId: "1069320271567",
+    appId: "1:1069320271567:web:ff0f634b8ea9d950d0e9ba"
 };
 
 const app = initializeApp(firebaseConfig);
